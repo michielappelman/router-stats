4385561fd52e2611ace832077ab82719a055c666
Michiel Appelman <michielappelman@gmail.com>
2012-12-06 00:45:45 +0100

    Initial Sync
    It's late, maybe I will find some bugs later but for now it seems to
    work in a once-a-minute cron-job.

---
+++ get-stats.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This script outputs the pretty XML with interface statistics from the router.
+#
+
+code=`curl http://192.168.0.1 2>/dev/null | grep -e ^auth_url | cut -d'=' -f4 | cut -d'"' -f1`
+salt=`curl http://192.168.0.1 2>/dev/null | grep "salt =" | cut -d'"' -f2`
+url=`node get-url.js $salt $code`
+
+curl $url >/dev/null 2>&1
+curl http://192.168.0.1/interface_stats.xml 2>/dev/null | tidy -xml 2>/dev/null
---
+++ parse-stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+"""
+This is the script that will eventually walk over the XML stats and 
+put the data in Graphite.
+"""
+
+import xml.etree.ElementTree as ET
+import subprocess
+import time
+from socket import socket 
+
+CARBON_SERVER = '127.0.0.1'
+CARBON_PORT = 2003
+
+stats = subprocess.check_output("get-stats.sh", shell=True)
+root = ET.fromstring(stats)
+now = str( time.time())
+
+lines = []
+
+interface = 0
+while interface <= 2:
+    stat=2
+    if interface == 2:
+        last = 6
+    else:
+        last = 7
+    while stat <= last:
+        lines.append("router." + root[interface][1].text.lower() + "." + root[interface][stat].tag + " " + root[interface][stat].text + " " + now )
+        stat += 1
+    interface += 1
+message = '\n'.join(lines) + '\n'
+
+sock = socket()
+try:
+    sock.connect( (CARBON_SERVER,CARBON_PORT) )
+except:
+    print "Couldn't connect to %(server)s on port %(port)d, is carbon-agent.py running?" % { 'server':CARBON_SERVER, 'port':CARBON_PORT }
+    sys.exit(1)
+
+sock.sendall(message)
+
