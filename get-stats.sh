b7c46d07fb0806bce94212ba6527fe5d27603ee8
Michiel Appelman <michielappelman@gmail.com>
2012-12-06 01:02:56 +0100

    Added the requirements to actually run the script.

--- get-stats.sh
+++ get-stats.sh
@@ -2,6 +2,11 @@
 #
 # This script outputs the pretty XML with interface statistics from the router.
 #
+# Requirements:
+#   - cURL
+#   - node.js
+#   - tidy (could be removed)
+
 
 code=`curl http://192.168.0.1 2>/dev/null | grep -e ^auth_url | cut -d'=' -f4 | cut -d'"' -f1`
 salt=`curl http://192.168.0.1 2>/dev/null | grep "salt =" | cut -d'"' -f2`
